0e70660782528cd86c5bb8d459b6807ad7bd59bb
davidjacob94 <davidjacob94@gmail.com>
2014-12-02 09:31:17 -0500

    python scripts to send int over serial
    3 Dec 2014

---
+++ trashcan.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import serial
+ser = serial.Serial(port = '/dev/tty', baudrate = 9600)  # open first serial port
+ser.write("0")      # write a string
+ser.close()             # close port

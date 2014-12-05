a8def904213738168a590390151fac89a99355bf
davidjacob94 <davidjacob94@gmail.com>
2014-12-05 02:20:04 -0500

    change file name/ organization
    5 Dec 2014

---
+++ gintonic.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import serial
+ser = serial.Serial(port = '/dev/tty', baudrate = 9600)  # open first serial port
+ser.write("2")      # write a string
+ser.close()             # close port
